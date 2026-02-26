fd168f9a2be5adad3e1a3e7da0f00288e0102718
vicpulsr <vicke.puls@hotmail.com>
2026-02-26 15:52:52 -0300

    feat: add E2B sandbox integration with code interpreter and update dependencies; include compile script and Dockerfile

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter"
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
+
     // Create a new agent with a system prompt (you can add optional tools, too)
     const summarizer = createAgent({
       name: "summarizer",
@@ -17,8 +24,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await summarizer.run(
       `Summarize the following text: ${event.data.value}`,
     );
-    console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 );
