a2440081e903e3924d5a1cd35c8798848dc9b52a
vicpulsr <vicke.puls@hotmail.com>
2026-03-02 11:12:23 -0300

    feat: add getOne procedure to fetch a single project by ID with error handling

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -4,8 +4,31 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import z from "zod";
 
 import { generateSlug } from "random-word-slugs";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "Project ID is required" }),
+      }),
+    )
+    .query(async ({ input }) => {
+      const existingProject = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!existingProject) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Project not found",
+        });
+      }
+
+      return existingProject;
+    }),
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
